b1b0301ca5afbf646787178cc7dc3eda11d6e82a
kirillsergeev <kirillovk317@gmail.com>
2023-02-04 18:22:38 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Даны 3 целых числа
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a > b:
+    a, b = b, a
+if b>c:
+    c, b = b, c
+if a>b:
+    a, b = b, a
+print(a, b, c)
+
+
+
+
+
+
